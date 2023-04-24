a136a335f60de74132593d5482bbb2b56094fa2d
Ejiofor Celestine Chinaza <121381551+CyberVerse2@users.noreply.github.com>
2023-04-24 21:54:35 +0100

    removed if statement

--- script.js
+++ script.js
@@ -4,20 +4,11 @@ let closeBtn = document.querySelector(".sidebar>img");
 let sidebarItems = document.querySelectorAll(".sidebar *");
 
 hamburger.addEventListener("click", function (e) {
-  // check if screen width is less than or equal to 375px
-  if (screen.width <= 480) {
-    // it matches
-    // hamburger.style.display = "none";
-    sidebar.style.display = "flex";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
-  }
-  // console.log("toggled");
+  sidebar.style.display = "flex";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 });
-console.log(sidebarItems);
+
 closeBtn.addEventListener("click", function () {
-  // check if screen width is less than or equal to 375px
-  if (screen.width <= 480) {
-    sidebar.style.display = "none";
-    document.body.style.backgroundColor = "var(--page-color)";
-  }
+  sidebar.style.display = "none";
+  document.body.style.backgroundColor = "var(--page-color)";
 });
