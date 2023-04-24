70e00da3f12886c7119f69d0f2232432794cec6f
Ejiofor Celestine Chinaza <121381551+CyberVerse2@users.noreply.github.com>
2023-04-24 21:51:34 +0100

    changed if statement in js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let sidebarItems = document.querySelectorAll(".sidebar *");
 
 hamburger.addEventListener("click", function (e) {
   // check if screen width is less than or equal to 375px
-  if (screen.width <= 375) {
+  if (screen.width <= 480) {
     // it matches
     // hamburger.style.display = "none";
     sidebar.style.display = "flex";
@@ -16,7 +16,7 @@ hamburger.addEventListener("click", function (e) {
 console.log(sidebarItems);
 closeBtn.addEventListener("click", function () {
   // check if screen width is less than or equal to 375px
-  if (screen.width <= 375) {
+  if (screen.width <= 480) {
     sidebar.style.display = "none";
     document.body.style.backgroundColor = "var(--page-color)";
   }
