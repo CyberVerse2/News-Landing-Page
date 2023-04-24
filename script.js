8ae6a39a4770f2d97a81db05ad5c44b0c3f65849
Ejiofor Celestine Chinaza <121381551+CyberVerse2@users.noreply.github.com>
2023-04-24 21:22:00 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+let hamburger = document.querySelector(".sidebar-toggle");
+let sidebar = document.querySelector(".sidebar");
+let closeBtn = document.querySelector(".sidebar>img");
+let sidebarItems = document.querySelectorAll(".sidebar *");
+
+hamburger.addEventListener("click", function (e) {
+  // check if screen width is less than or equal to 375px
+  if (screen.width <= 375) {
+    // it matches
+    // hamburger.style.display = "none";
+    sidebar.style.display = "flex";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+  }
+  // console.log("toggled");
+});
+console.log(sidebarItems);
+closeBtn.addEventListener("click", function () {
+  // check if screen width is less than or equal to 375px
+  if (screen.width <= 375) {
+    sidebar.style.display = "none";
+    document.body.style.backgroundColor = "var(--page-color)";
+  }
+});
